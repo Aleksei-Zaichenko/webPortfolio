b213cc9d740268109fd8de3eea1b3bcc67df2cc4
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-06-17 14:35:10 -0400

    Changes the structure of App.js and modifies the styling of the nav bar

--- webportolio/src/App.js
+++ webportolio/src/App.js
@@ -9,31 +9,28 @@ import ContactMepage from "./components/ContactMePage";
 
 //react bootstrap components
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col, Navbar } from "react-bootstrap";
+import { Container, Navbar } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <Navbar bg="dark" variant="dark">
+      <Navbar
+        className="custom-navbar-style"
+        bg="dark"
+        variant="dark"
+        sticky="top"
+      >
+        <Container>
           <Navbar.Brand href="#home">Home</Navbar.Brand>
-          <Navbar.Brand href="#home">Projects</Navbar.Brand>
-          <Navbar.Brand href="#home">About Me</Navbar.Brand>
-          <Navbar.Brand href="#home">Contact</Navbar.Brand>
-        </Navbar>
-        <Row>
-          <HomePage />
-        </Row>
-        <Row>
-          <AboutMePage />
-        </Row>
-        <Row>
-          <PortfolioProjectPage />
-        </Row>
-        <Row>
-          <ContactMepage />
-        </Row>
-      </Container>
+          <Navbar.Brand href="#projects">Projects</Navbar.Brand>
+          <Navbar.Brand href="#aboutme">About Me</Navbar.Brand>
+          <Navbar.Brand href="#contact">Contact</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <HomePage />
+      <AboutMePage />
+      <PortfolioProjectPage />
+      <ContactMepage />
     </div>
   );
 }
