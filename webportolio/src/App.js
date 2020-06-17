7850fb17cbdd6cd2a63e8f764ec0d22e3ecdf31c
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-06-17 13:52:24 -0400

    Adds a nav bar component from Bootstrap library

--- webportolio/src/App.js
+++ webportolio/src/App.js
@@ -9,16 +9,30 @@ import ContactMepage from "./components/ContactMePage";
 
 //react bootstrap components
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Navbar } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <Container>
-        <HomePage />
-        <AboutMePage />
-        <PortfolioProjectPage />
-        <ContactMepage />
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Home</Navbar.Brand>
+          <Navbar.Brand href="#home">Projects</Navbar.Brand>
+          <Navbar.Brand href="#home">About Me</Navbar.Brand>
+          <Navbar.Brand href="#home">Contact</Navbar.Brand>
+        </Navbar>
+        <Row>
+          <HomePage />
+        </Row>
+        <Row>
+          <AboutMePage />
+        </Row>
+        <Row>
+          <PortfolioProjectPage />
+        </Row>
+        <Row>
+          <ContactMepage />
+        </Row>
       </Container>
     </div>
   );
