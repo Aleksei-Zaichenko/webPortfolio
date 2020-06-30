852788b275a533b32d7d5b440edda99da09c9ab4
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-06-30 16:57:06 -0400

    Modifies project card in portfolioProjectsPage.js

--- webportolio/src/components/PortfolioProjectsPage.js
+++ webportolio/src/components/PortfolioProjectsPage.js
@@ -2,15 +2,32 @@ import React, { useState } from "react";
 
 //Imports for the style
 import "./styles/PortfolioProjectsPage.css";
+import spotifySongsSuggester from "./images/spotifySuggester.jpg";
 
 import { CardGroup, Card, ListGroup } from "react-bootstrap";
 
 function PortfolioProjectsPage() {
   const [projects, setProjects] = useState([
-    "first",
-    "second",
-    "third",
-    "fourth",
+    {
+      title: "Spotify Songs Suggester",
+      img: spotifySongsSuggester,
+      description:
+        "This app recommends a user the songs based on the searched ones. This project was created during one of build weeks in Lambda programming bootcamp. \
+        When it was time to handle in that app it was not ready even for 50%. \
+        I got very interested and passinate about that project and completed as much as I could on my own. Initially I was backend developer for that project.",
+      deployedLink:
+        "https://build-week-spotify-song-recommender.github.io/front-end/#/home",
+      githubRepo: "https://github.com/Build-Week-Spotify-Song-Recommender",
+      usedTechnologies: [
+        "React",
+        "Redux",
+        "Material UI",
+        "Node",
+        "Express",
+        "SQL",
+        "Knex",
+      ],
+    },
   ]);
 
   return (
@@ -22,15 +39,25 @@ function PortfolioProjectsPage() {
         {projects.map((project) => (
           <Card bsPrefix="singleCard">
             <Card.Body className="cardBody">
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Title>{project}</Card.Title>
+              <Card.Img variant="top" src={project.img} />
+              <Card.Title style={{ margin: "3% 0" }}>
+                {project.title}
+              </Card.Title>
+              <a
+                style={{ display: "block", margin: "2% 0" }}
+                href={project.deployedLink}
+              >
+                Deployed Link
+              </a>
+              <a href={project.githubRepo}>GitHub repository</a>
             </Card.Body>
             <Card.Footer className="cardFooter">
-              <Card.Text>jsklndflskdnf;lskdnf'lksdnf'lskdnf'skdnf</Card.Text>
+              <Card.Text>{project.description}</Card.Text>
               <div className="listOfTechnologies">Technologies used:</div>
               <ListGroup>
-                <ListGroup.Item>React</ListGroup.Item>
-                <ListGroup.Item>Bootstrap</ListGroup.Item>
+                {project.usedTechnologies.map((technology) => (
+                  <ListGroup.Item>{technology}</ListGroup.Item>
+                ))}
               </ListGroup>
             </Card.Footer>
           </Card>
